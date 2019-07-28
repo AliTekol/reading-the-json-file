65891c246eecfce8facad8df83f5e7fad41ac3c5
AliTekol <alitekol65@gmail.com>
2019-07-28 15:08:31 +0300

    first

---
+++ game_list.py
@@ -0,0 +1,13 @@
+import json
+import os
+
+
+#data = json.load(open("C:\\Users\\Ali\\Desktop\\ali\\prg\\games\\games.json"))
+
+print(os.getcwd())
+
+#with open('games.json', 'r') as f:
+ #   gamelist = json.load(f)
+
+#for game in gamelist:
+    #print(game['name'])
